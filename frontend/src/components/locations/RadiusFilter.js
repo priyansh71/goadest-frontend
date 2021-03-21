cc650b0a3650db9af63ebe07e29d2233f88cd7d6
AnirudhM1 <mukherjeeanirudh100@gmail.com>
2021-03-21 18:57:04 +0530

    commit

--- frontend/src/components/locations/RadiusFilter.js
+++ frontend/src/components/locations/RadiusFilter.js
@@ -1,9 +1,10 @@
 
 const RadiusFilter = ({onChange, radius}) => {
     return (
-        <div>
+        <label>
+            Radius:
             <input type='range' min='1' max='100' value={radius} onChange={onChange}/>
-        </div>
+        </label>
     )
 }
 
