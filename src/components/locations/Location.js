d06c2a5219362804598f47d2b18d5119a366ec3f
Anirudh Mukherjee <mukherjeeanirudh100@gmail.com>
2021-03-26 21:15:54 +0530

    Added Locations card styling

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -16,13 +16,25 @@ const Location = () => {
 	const location = { distance, typeofplace: option };
 
 	const handleChange_distance = (e) => {
+        const flag = (e.target.value !== distance);
 		setDistance(e.target.value);
+        if(flag == true) {
+            handleFindDest();
+        }
 	};
 	const handleChange_option = (e) => {
+        const flag = (e.target.value !== option);
 		setOption(e.target.value);
+        if(flag == true) {
+            handleFindDest();
+        }
 	};
 	const handleSearchChange = (e) => {
+        const flag = (e.target.value !== searchValue);
 		setSearchValue(e.target.value);
+        if(flag == true) {
+            handleFindDest();
+        }
 	};
 
 	const handleFindDest = () => {
@@ -42,21 +54,34 @@ const Location = () => {
 			});
 	};
 
+    const tempLoc = [
+        {
+            key: 1,
+            placename: 'Name',
+            address: 'address',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni pariatur necessitatibus nesciunt dignissimos illo sapiente consectetur non totam. Doloribus et quia nam suscipit nihil vero sequi nobis aut ut modi?',
+            typeofplace: 'beach',
+            latitude: 15.353334767659915,
+            longitude: 73.7778551695931,
+            image: 'https://images.unsplash.com/photo-1597820334272-af87b2d917c1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1383&q=80',
+
+        },
+    ]
+
 	return (
-		<div>
+		<div className={Styles.locationContainer}>
 			<div className={Styles.searchHolder}>
 				<Search value={searchValue} onChange={handleSearchChange} />
 			</div>
 			<section className={Styles.container}>
 				<div className={Styles.main}>
-					<MainSection locations={locations} />
+					<MainSection locations={tempLoc} />
 				</div>
 				<div className={Styles.filters}>
 					<h4>Filters</h4>
 					<RadiusFilter onChange={handleChange_distance} distance={distance} />
 					<OptionFilter onChange={handleChange_option} option={option} />
 				</div>
-				<button onClick={handleFindDest}> Find Destinations </button>
 			</section>
 		</div>
 	);
--- src/components/locations/LocationData.js
+++ src/components/locations/LocationData.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Styles from './Location.module.css'
 import getDistfromCoords from '../../getdistance'
 
-function LocationData({ placename, address, description, typeofplace, latitude, longitude }) {
+function LocationData({ placename, address, description, typeofplace, image, latitude, longitude }) {
 
-    const distance = Math.trunc(getDistfromCoords(latitude.$numberDecimal, longitude.$numberDecimal))
+    const distance = 10;//Math.trunc(getDistfromCoords(latitude.$numberDecimal, longitude.$numberDecimal))
 
     return (
         <div className={Styles.cell}>
@@ -12,19 +12,19 @@ function LocationData({ placename, address, description, typeofplace, latitude, 
             <div className={Styles.imgContainer}>
                 <img
                     className={Styles.img}
-                    src='https://images.unsplash.com/photo-1597820334272-af87b2d917c1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1383&q=80'
+                    src={image}
                 />
             </div>
 
             <div className={Styles.textContainer}>
                 <h2>{placename}</h2>
                 <p>{description}</p>
-                <p>Type of location: <b>{typeofplace}</b></p>
-                <p>Distance from campus: <b>{distance}</b></p>
-                <p>{address}</p>
+                <p>Type of location:{typeofplace} <br /> Distance from campus: {distance}</p>
+                <p>Address: {address}</p>
             </div>
 
             <iframe 
+                title={placename}
                 className={Styles.map}
                 src={`https://maps.google.com/maps?q=${latitude},${longitude}&hl=en&z=12&amp;&output=embed`}
                 width="150" 
