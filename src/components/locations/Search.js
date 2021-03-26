7e5486c83f92e02159477bf691937449adbdf975
Anirudh Mukherjee <mukherjeeanirudh100@gmail.com>
2021-03-26 23:41:37 +0530

    Merged Priyanshs changes

--- src/components/locations/Search.js
+++ src/components/locations/Search.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import styles from './Location.module.css'
 import LocationData from './LocationData';
+import config from '../../config'
 
 const Search = ({ value, onChange }) => {
 
