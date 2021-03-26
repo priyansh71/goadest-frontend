2a58844b8d2c2a7f06db0504fc0eccb89ca543ae
=kanishq1 <=kanishqkhandelwal@gmail.com>
2021-03-27 00:01:16 +0530

    search file update

--- src/components/locations/Search.js
+++ src/components/locations/Search.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import { config } from "../../config";
 import styles from "./Location.module.css";
 import LocationData from "./LocationData";
 
