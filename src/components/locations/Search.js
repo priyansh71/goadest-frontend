85c1b7d5ab3e7385f9742825ab714e7a34f8f721
=kanishq1 <=kanishqkhandelwal@gmail.com>
2021-03-26 23:56:05 +0530

    search file update

--- src/components/locations/Search.js
+++ src/components/locations/Search.js
@@ -1,50 +1,50 @@
-import axios from 'axios';
-import { useState } from 'react';
-import styles from './Location.module.css'
-import LocationData from './LocationData';
-import config from '../../config'
+import axios from "axios";
+import { useState } from "react";
+import styles from "./Location.module.css";
+import LocationData from "./LocationData";
 
 const Search = ({ value, onChange }) => {
+	const [location, setLocation] = useState("");
 
-    const [location, setLocation] = useState('')
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+		axios({
+			url: `${config.backendUrl}/post`,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: {
+				userSearch: value,
+			},
+		})
+			.then((res) => {
+				setLocation(res.data);
+			})
+			.catch((e) => {
+				console.log(e.message);
+			});
+	};
 
-        axios({
-            url: `${config.backendUrl}/post`,
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            data: {
-                userSearch: value
-            }
-        }).then((res) => {
-            setLocation(res.data)
-        }).catch((e) => {
-            console.log(e.message)
-        })
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-
-                <input className={styles.search} type='text' value={value} onChange={onChange} />
-                <input id="button" type='submit' value='Submit' />
-            </form>
-            {location && <LocationData
-                placename={location.placename}
-                address={location.address}
-                description={location.description}
-                typeofplace={location.typeofplace}
-                latitude={location.latitude}
-                longitude={location.longitude}
-            />}
-        </div>
-    )
-
-}
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input className={styles.search} type="text" value={value} onChange={onChange} />
+				<input id="button" type="submit" value="Submit" />
+			</form>
+			{location && (
+				<LocationData
+					placename={location.placename}
+					address={location.address}
+					description={location.description}
+					typeofplace={location.typeofplace}
+					latitude={location.latitude}
+					longitude={location.longitude}
+				/>
+			)}
+		</div>
+	);
+};
 
 export default Search;
